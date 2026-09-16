4c0b18148b4b440014511d975ca403166554bfa9
TheTravellingVetPhysio <julietherold@hotmail.com>
2026-09-16 22:29:20 +0200

    CliApp started, DI in Program.cs

---
+++ Server/CLI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Entities;
+using RepositoryContracts;
+using InMemoryRepositories;
+using CLI.UI;
+
+Console.WriteLine("Starting CLI app...");
+
+IUserRepository userRepository = new UserInMemoryRepository();
+IPostRepository postRepository = new PostInMemoryRepository();
+ICommentRepository commentRepository = new CommentInMemoryRepository();
+IReactionRepository reactionRepository = new ReactionInMemoryRepository();
+ISubforumRepository subforumRepository = new SubforumInMemoryRepository();
+
+await DataSeeder.SeedAsync(
+    userRepository,
+    postRepository,
+    commentRepository,
+    reactionRepository,
+    subforumRepository);
+
+CliApp cliApp = new CliApp(
+    userRepository,
+    postRepository,
+    commentRepository,
+    reactionRepository,
+    subforumRepository);
+
+await cliApp.RunAsync();
