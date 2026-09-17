8881a7fc8bd6ea0e1c9fa20d7e4be7de50dcaff5
TheTravellingVetPhysio <julietherold@hotmail.com>
2026-09-17 08:19:23 +0200

    CliApp

--- Server/CLI/UI/CliApp.cs
+++ Server/CLI/UI/CliApp.cs
@@ -2,5 +2,28 @@ namespace CLI.UI;
 
 public class CliApp
 {
-    // ...
+    private readonly IUserRepository userRepository;
+    private readonly IPostRepository postRepository;
+    private readonly ICommentRepository commentRepository;
+    private readonly IReactionRepository reactionRepository;
+    private readonly ISubforumRepository subforumRepository;
+
+    public CliApp(
+        IUserRepository userRepository,
+        IPostRepository postRepository,
+        ICommentRepository commentRepository,
+        IReactionRepository reactionRepository,
+        ISubforumRepository subforumRepository)
+    {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+        this.reactionRepository = reactionRepository;
+        this.subforumRepository = subforumRepository;
+    }
+
+    public async Task RunAsync()
+    {
+        
+    }
 }
